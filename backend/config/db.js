5a435f6b755fd07f30883b0d2d2d4975f17098cd
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2024-11-25 14:45:35 -0300

    troca de dbs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-//adicionado pool para suporte a promise
+/* //adicionado pool para suporte a promise
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -11,6 +11,22 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+ */
+
+// Configuração do banco (só para teste)
+const pool = mysql.createPool({
+  host: 'blugeddj68mfwddszfyw-mysql.services.clever-cloud.com', 
+  user: 'u7zv9tojrhhp4uit',           
+  password: 'ceePfgbPJuVpdWHK550X',        
+  database: 'blugeddj68mfwddszfyw',         
+  port: 3306,                  
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+
+
 
 pool.getConnection()
   .then(connection => {
