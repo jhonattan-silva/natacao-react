ddf7a42b411ac8d93f8c72530fba7cdf548cd1d0
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2025-01-25 11:05:22 -0300

    nova abordagem front compose 2

---
+++ wait-for.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for.sh
+
+set -e
+
+host="$1"
+shift
+port="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  echo "Waiting for $host:$port..."
+  sleep 1
+done
+
+echo "$host:$port is available, executing command"
+exec $cmd
