2142d6ffc78ac0442b4c05c643c53384ce45904f
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2025-01-27 09:08:15 -0300

    css rodape e cabeçalho - correção api etapas

--- frontend/src/componentes/Cabecalho/Cabecalho.js
+++ frontend/src/componentes/Cabecalho/Cabecalho.js
@@ -1,24 +1,32 @@
 import { Link } from 'react-router-dom';
 import style from './Cabecalho.module.css';
 import CabecalhoLink from '../CabecalhoLink/CabecalhoLink';
+import { useState } from 'react';
 
 const logo = ['./imagens/logo_noBG.png'];
 
 const Cabecalho = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className={style.cabecalho}>
             <Link to="./">
                 <img src={logo} alt='LPN logo' className={style.logo}></img>
             </Link>
-            <nav>
+            <span className={style.menuIcon} onClick={toggleMenu}>&#9776;</span>
+            <nav className={menuOpen ? style.open : ''}>
                 <CabecalhoLink url='./Noticias'> Noticias </CabecalhoLink>
                 <CabecalhoLink url='./Etapas'> Etapas </CabecalhoLink>
                 <CabecalhoLink url='./Resultados'> Resultados </CabecalhoLink>
                 <CabecalhoLink url='./Classificacao'> Classificação </CabecalhoLink>
                 <CabecalhoLink url='./balizamento'> Balizamento </CabecalhoLink>
                 <CabecalhoLink url='./rankings'>Rankings</CabecalhoLink>
+                <CabecalhoLink url='./Admin'> Acesso Restrito </CabecalhoLink>
             </nav>
-            <CabecalhoLink url='./Admin'> Acesso Restrito </CabecalhoLink>
         </header>
     )
 }
