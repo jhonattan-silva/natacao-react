623e1f8fb4f4c67bdc9c5ba549fecb946fcb2a99
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2025-02-22 10:07:44 -0300

    aumentado limite para balizamento no server bodyparser

--- backend/server.js
+++ backend/server.js
@@ -93,7 +93,10 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(bodyParser.json());
+// Configura o body parser com um limite maior para o JSON e urlencoded
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(helmet());
 
 app.get('/', (req, res) => {
