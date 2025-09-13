bc858e65be2572739ca8a166881b4fd5417b103c
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2025-09-13 09:59:08 -0300

    correção do server nginx

--- backend/server.js
+++ backend/server.js
@@ -164,12 +164,12 @@ app.use('/uploads', (req, res, next) => {
 }));
 
 // Servir o frontend em produção
-if (process.env.NODE_ENV === 'production') {
+/* if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
-}
+} */
 
 // Middleware para página não encontrada
 app.use((req, res) => {
